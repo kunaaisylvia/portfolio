0f00073ddf826b5c936fa7db1660425f54cf900b
kunaaisylvia <kunaaisylvia@zetech.ac.ke>
2021-10-15 17:01:47 +0300

    add utilities html file and index and idex js file

---
+++ js/index.js
@@ -0,0 +1,23 @@
+function calculator(num1, num2)
+const num1 = document.getElementById(num1);
+const num2 = document.getElementById(num2);
+
+num1 = parseInt(num1.value);
+num2 = parseInt(num2.value);
+
+
+let num1 = 200;
+let num2 = 30;
+n
+const sum = num1 + num2;
+const diff = num1 + num2;
+const product = num1 * num2;
+const quot = num1 / num2;
+
+console.log("sum: " + sum);
+console.log("difference: " + diff);
+console.log("product: " + product);
+console.log("quotient: " + quot);
+console.log("typeof quotient: " + typeof quot);
+
+calculator(200, 56);
